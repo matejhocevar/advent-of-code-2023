6a1b7aa6ca2e081246d3d08e502ebc9532238db1
Matej Hočevar <matej.hocevar@tretton37.com>
2023-12-08 00:29:21 +0100

    Implement Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (line in input) {
+            val num = line.replace(Regex("[A-Za-z]"), "")
+            val first: Int = num.firstOrNull()?.digitToInt() ?: 0
+            val last: Int = num.lastOrNull()?.digitToInt() ?: first
+            sum += "$first$last".toInt()
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return part1(input.map {
+            it
+                    .replace("one", "o1e")
+                    .replace("two", "t2o")
+                    .replace("three", "t3e")
+                    .replace("four", "f4r")
+                    .replace("five", "f5e")
+                    .replace("six", "s6x")
+                    .replace("seven", "s7n")
+                    .replace("eight", "e8t")
+                    .replace("nine", "n9e")
+        })
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
